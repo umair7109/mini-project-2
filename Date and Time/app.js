97789cde72f046c6b7eb8fc134515439fb40dc6d
umair7109 <muhammadumair2354@gmail.com>
2026-05-09 00:34:18 +0500

    date and time in progress

---
+++ Date and Time/app.js
@@ -0,0 +1,9 @@
+// 1. Custom Date Formatter
+// Write a function that takes a Date object and returns a formatted string like:
+// "Saturday, 9 August 2025".
+
+function todayDate(){
+    return new Date
+}
+
+todayDate();
